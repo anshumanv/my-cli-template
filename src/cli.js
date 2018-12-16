9cc22cd281e47d38f1b094ecacb0584157aa2e87
Anshuman Verma <anshu.av97@gmail.com>
2018-12-16 10:01:48 +0530

    init :tada:

---
+++ src/cli.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+'use strict';
+const path = require('path');
+const meow = require('meow');
+const termImg = require('term-img');
+const got = require('got');
+const terminalImage = require('terminal-image');
+const importJsx = require('import-jsx');
+const {h, render} = require('ink');
+
+const ui = importJsx('./ui');
+
+meow(`
+	Usage
+	  $ {{ github }}
+`);
+
+const fallback = async () => {
+  const { avatar_url } = await got('api.github.com/users/{{ github }}');
+  const { body } = await got(avatar_url, {encoding: null});
+	const image = await terminalImage.buffer(body);
+	console.log(image);
+};
+
+(async () => {
+	const { avatar_url } = await got('api.github.com/users/{{ github }}');
+  const { body } = await got(avatar_url, {encoding: null});
+  termImg(body, {fallback});
+
+})();
+
+render(h(ui));
